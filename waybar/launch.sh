59287d179b5b4288d4fbbbeb2b2f5263d44c4222
Deepesh Sunuwar <deepgeneral33@gmail.com>
2026-04-24 15:05:05 +0545

    feat: minor color tweaks in swaync

--- waybar/launch.sh
+++ waybar/launch.sh
@@ -2,14 +2,14 @@
 
 killall -9 waybar
 killall -9 swaync
-killall -9 hyprsunset
+# killall -9 hyprsunset
 killall -9 blueman-applet
 killall -9 hyprshot
 # killall -9 hyprpaper
 
 waybar &
 swaync &
-hyprsunset &
+# hyprsunset &
 blueman-applet &
 hyprshot &
 # hyprpaper &
